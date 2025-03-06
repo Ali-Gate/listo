7dad004f2d280fadd99619be59cca231eb0ae45a
Ali Gate <ali_also@yahoo.es>
2025-03-06 17:05:09 +0000

    intalled gunicorn, set allowed hosts

--- listo/settings.py
+++ listo/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-mxd@lzwrpxognya=$f*t4*$j&ov@m07xqym0y1gjzt7ott-y)e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', ]
 
 
 # Application definition
