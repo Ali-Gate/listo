c8af49cdd47272b3c215c3e6b719e6b9df8844b8
Ali Gate <ali_also@yahoo.es>
2025-03-16 03:21:19 +0000

    working on responsice design: adjusted header and footer, fixed them, create nedia queries for screen sized (centering content: still working on vertical alignment for small screens), and addressed overflow in y so no space for scroll bas shows.

--- listo_app/admin.py
+++ listo_app/admin.py
@@ -1,7 +1,29 @@
 from django.contrib import admin
-from .models import TodoItem, User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import TodoItem
+from django.contrib.auth.models import User
+from django.urls import reverse
+from django.utils.html import format_html
 
+class TodoItemAdmin(admin.ModelAdmin):
+    list_display = ('title', 'due_date', 'completed', 'owner')
+    search_fields = ('title', 'owner__username')
+    list_filter = ('completed', 'due_date', 'owner')
+
+class TodoItemInline(admin.TabularInline):
+    model = TodoItem
+    extra = 1
+
+class CustomUserAdmin(BaseUserAdmin):
+    inlines = [TodoItemInline]
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'view_tasks_link')
+
+    def view_tasks_link(self, obj):
+        url = reverse('admin:listo_app_todoitem_changelist') + f'?owner__id__exact={obj.id}'
+        return format_html('<a href="{}">View Tasks</a>', url)
+    view_tasks_link.short_description = 'Tasks'
 
 # Register your models here.
-admin.site.register(TodoItem)
-admin.site.register(User) # Added this line
+admin.site.register(TodoItem, TodoItemAdmin)  # Register TodoItem with TodoItemAdmin
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin) # Added this line, Register User with CustomUserAdmin
--- listo_app/views.py
+++ listo_app/views.py
@@ -27,7 +27,7 @@ def user_login(request):
 
 @login_required
 def todo_list(request):
-    todos = TodoItem.objects.all()
+    todos = TodoItem.objects.filter(owner=request.user) 
     return render(request, 'todo/todo_list.html', {'todos': todos})
 
 def user_logout(request):
@@ -48,14 +48,16 @@ def todo_create(request):
     if request.method == 'POST':
         form = TodoItemForm(request.POST)
         if form.is_valid():
-            form.save()
+            todo = form.save(commit=False)
+            todo.owner = request.user  # Set the owner to the logged-in user
+            todo.save()
             return redirect('todo_list')
     else:
         form = TodoItemForm()
     return render(request, 'todo/todo_form.html', {'form': form})
 
 def todo_update(request, pk):
-    todo = get_object_or_404(TodoItem, pk=pk)
+    todo = get_object_or_404(TodoItem, pk=pk, owner=request.user) # Ensure the task belongs to the logged-in user
     if request.method == 'POST':
         form = TodoItemForm(request.POST, instance=todo)
         if form.is_valid():
@@ -66,7 +68,7 @@ def todo_update(request, pk):
     return render(request, 'todo/todo_form.html', {'form': form})
 
 def todo_delete(request, pk):
-    todo = get_object_or_404(TodoItem, pk=pk)
+    todo = get_object_or_404(TodoItem, pk=pk, owner=request.user)  # Ensure the task belongs to the logged-in user
     if request.method == 'POST':
         todo.delete()
         return redirect('todo_list')
