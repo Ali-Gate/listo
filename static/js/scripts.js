4b56a0045b1f3aeb4852b4fdf7e30472eb61232b
Ali Gate <ali_also@yahoo.es>
2025-03-23 22:44:28 +0000

    added a condition to modal to fix backdrop loading error

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,25 +1,28 @@
 // filepath: c:\Users\Ali\Documents\VS_CODE projects\listo\static\js\scripts.js
 
-const modal = new bootstrap.Modal(document.getElementById('staticBackdrop'));
 const form = document.getElementById('todoForm');
 
+if (form) {
+    //document.activeElement.blur(); // Remove focus from the submit button
+    const modal = new bootstrap.Modal(document.getElementById('staticBackdrop'));
 
-form.addEventListener('submit', function(event) {
-    const dueDateInput = document.querySelector('input[type="date"]');
-    const dueDate = new Date(dueDateInput.value);
-    const today = new Date();
-    today.setHours(0, 0, 0, 0); // Set time to midnight to compare only dates
+    form.addEventListener('submit', function(event) {
+        const dueDateInput = document.querySelector('input[type="date"]');
+        const dueDate = new Date(dueDateInput.value);
+        const today = new Date();
+        today.setHours(0, 0, 0, 0); // Set time to midnight to compare only dates
 
-    if (dueDate < today) {
-        event.preventDefault(); // Prevent form submission
-        modal.show();
-    }
-});
+        if (dueDate < today) {
+            event.preventDefault(); // Prevent form submission
+            modal.show();            
+        }
+    });
 
-const submitButton = document.getElementById("confirm-submit");
-submitButton.addEventListener('click', function() {
-    form.submit();
-});
+    const submitButton = document.getElementById("confirm-submit");
+    submitButton.addEventListener('click', function() {
+        form.submit();
+    });
+}
 
 document.addEventListener('DOMContentLoaded', function() {
     // Handle alert messages
